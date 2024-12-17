68f0943a4431ca153ac67d5acef060196c7a2ca5
kelly <maykelanuge@gmail.com>
2024-12-17 21:51:51 +0800

    final commit ig

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental',
-  }
+  },  
 };
-module.exports = {
-  basePath: '/nextjs-dashboard'
-}
 export default nextConfig;
